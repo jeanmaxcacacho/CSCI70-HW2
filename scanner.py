7ec1e62a2c6e50046469ebf4e23326e56e477e13
jeanmaxcacacho <maxcacacho@gmail.com>
2025-08-21 10:02:56 +0800

    update: transition table done

--- scanner.py
+++ scanner.py
@@ -32,7 +32,6 @@ class State(Enum):  # system can only be any one of these states at a time
     EOF = "EOF"
 
 
-# TODO: finish tran table
 # transitions[current_state][char] -> (next_state, emit_token, need_pushback)
 transitions = {
     State.BLANK: {
@@ -43,19 +42,20 @@ transitions = {
         "-": (State.MINUS, "MINUS", False),
         "numeric": (State.NUM, "NUM", False),
     },
-    State.NUM: {  # TODO: this part of the tran table still isn't done
-        "blank": (State.BLANK, "NUM", False),
+    State.NUM: {  # digit keep continuing, but if not then go back
+        "blank": (State.BLANK, "NUM", False),  # ignore whitespace
         "other": (State.ERROR, None, False),
-        "=": (State.ASSIGN, None, False),
-        "+": (State.PLUS, "PLUS", False),
-        "-": (State.MINUS, "MINUS", False),
-        "numeric": (State.NUM, ),
+        "": (State.EOF, "NUM", False),
+        "=": (State.ASSIGN, None, True),
+        "+": (State.PLUS, "PLUS", True),
+        "-": (State.MINUS, "MINUS", True),
+        "numeric": (State.NUM, None, False),
     },
     State.ASSIGN: {
-        "other": (State.ERROR, None),  # anything else that isn't '='
-        "=": (State.ASSIGN, "EQUALS"),
+        "other": (State.ERROR, None, False),  # anything else that isn't '='
+        "=": (State.ASSIGN, "EQUALS", False),
     },
-    State.PLUS: {
+    State.PLUS: {  # digits can follow one after another
         "blank": (State.BLANK, None, False),
         "other": (State.ERROR, None, False),
         "=": (State.ASSIGN, None, False),
@@ -64,12 +64,12 @@ transitions = {
         "numeric": (State.NUM, "NUM", False),
     },
     State.MINUS: {
-        "blank": (State.BLANK, None),
-        "other": (State.ERROR, None),
-        "=": (State.ASSIGN, None),
-        "+": (State.PLUS, "PLUS"),
-        "-": (State.MINUS, "MINUS"),
-        "numeric": (State.NUM, "NUM"),
+        "blank": (State.BLANK, None, False),
+        "other": (State.ERROR, None, False),
+        "=": (State.ASSIGN, None, False),
+        "+": (State.PLUS, "PLUS", False),
+        "-": (State.MINUS, "MINUS", False),
+        "numeric": (State.NUM, "NUM", False),
     },
     State.ERROR: {},  # no transitions here
     State.EOF: {}  # no transitions here also
@@ -88,8 +88,8 @@ output_file = io.open("output.txt", "w")
 while current_state not in (State.ERROR, State.EOF):
     # read a character, assign currentCharacter
     currentCharacter = input_file.read(1)
-    # if currentCharacter == "":
-    #     currentState = State.EOF
+    if currentCharacter == "":
+        currentState = State.EOF
     # write to buffer, if state transition happens flush buffer to output_file
     # output_file.write(currentCharacter)
 
