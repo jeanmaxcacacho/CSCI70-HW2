f4c416d06054dfaac71042e19688ae01676f58fa
jeanmaxcacacho <maxcacacho@gmail.com>
2025-08-24 10:51:47 +0800

    i fucking hate myself

--- scanner.py
+++ scanner.py
@@ -7,14 +7,18 @@ general rules of DFA (from the graph):
 general notes for program
 - read the file character by character
 - write buffers to file not characters, flush per transition
+
+let's rigorously define pushback behaviour
+- if transitioning out of NUM to a valid state, flush current buffer contents
+- then append current character to the current buffer, "reread" current char
 """
 
 import sys
 import io
 from enum import Enum
 
-if len(sys.argv) != 2:
-    print(f"[ERROR] Usage: python {sys.argv[0]} <input_file>")
+if len(sys.argv) != 3:
+    print(f"Usage: python {sys.argv[0]} <input_file> <output_file>")
     sys.exit(1)
 
 
@@ -28,10 +32,10 @@ class State(Enum):  # system can only be any one of these states at a time
     EOF = "EOF"
 
 
-def group_char(c):  # utility function to get character key
+def gk(c):  # get key to shove in to t_table
     if c.isnumeric():
         return "numeric"
-    elif c in ("=", "+", "-"):
+    elif c in ("=", "+", "-", ""):
         return c
     elif c.isspace():
         return "blank"
@@ -39,83 +43,124 @@ def group_char(c):  # utility function to get character key
         return "other"
 
 
-# transitions[current_state][char] -> (next_state, emit_token, need_pushback)
+# TODO: fix _token: double check t_table
+# t_table[state][char] -> (_state, _token, _pushback)
+# _state: following state, _token: token ID of char, _pushback: pushback bool
 t_table = {
-    State.BLANK: {
+    State.BLANK: {  # catch-all state
         "blank": (State.BLANK, None, False),
         "other": (State.ERROR, None, False),
         "=": (State.ASSIGN, None, False),
-        "+": (State.PLUS, "PLUS", False),
-        "-": (State.MINUS, "MINUS", False),
-        "numeric": (State.NUM, "NUM", False),
-    },
-    State.NUM: {  # digit keep continuing, but if not then go back
-        "blank": (State.BLANK, "NUM", False),  # ignore whitespace
-        "other": (State.ERROR, None, False),
-        "": (State.EOF, "NUM", False),
-        "=": (State.ASSIGN, None, True),
-        "+": (State.PLUS, "PLUS", True),
-        "-": (State.MINUS, "MINUS", True),
+        "+": (State.PLUS, None, False),
+        "-": (State.MINUS, None, False),
         "numeric": (State.NUM, None, False),
+        "": (State.EOF, None, False),
     },
-    State.ASSIGN: {
+    State.NUM: {  # digits accumulate, shifts to other states cut that number
+        "blank": (State.BLANK, "NUM", False),
+        "other": (State.ERROR, "NUM", False),
+        "=": (State.BLANK, "NUM", True),  # blank state to account for pushback
+        "+": (State.BLANK, "NUM", True),
+        "-": (State.BLANK, "NUM", True),
+        "numeric": (State.NUM, None, False),  # keep accumulating
+        "": (State.EOF, "NUM", False),
+    },
+    State.ASSIGN: {  # already have first '=', only valid is another '='
         "blank": (State.ERROR, None, False),
         "other": (State.ERROR, None, False),
+        "=": (State.BLANK, "ASSIGN", False),  # only at second '='
+        "+": (State.ERROR, None, False),
+        "-": (State.ERROR, None, False),
+        "numeric": (State.ERROR, None, False),
         "": (State.ERROR, None, False),
-        "=": (State.ASSIGN, None, True),
-        "+": (State.ERROR, None, True),
-        "-": (State.ERROR, None, True),
-        "numeric": (State.NUM, None, False),
     },
-    State.PLUS: {  # digits can follow one after another
-        "blank": (State.BLANK, None, False),
-        "other": (State.ERROR, None, False),
-        "=": (State.ASSIGN, None, False),
+    State.PLUS: {  # '+' and '-' can be in series, same transition rules
+        "blank": (State.BLANK, "PLUS", False),
+        "other": (State.ERROR, "PLUS", False),
+        "=": (State.ASSIGN, "PLUS", False),
         "+": (State.PLUS, "PLUS", False),
-        "-": (State.MINUS, "MINUS", False),
-        "numeric": (State.NUM, "NUM", False),
+        "-": (State.MINUS, "PLUS", False),
+        "numeric": (State.NUM, "PLUS", False),
+        "": (State.EOF, "PLUS", False),
     },
     State.MINUS: {
-        "blank": (State.BLANK, None, False),
-        "other": (State.ERROR, None, False),
-        "=": (State.ASSIGN, None, False),
-        "+": (State.PLUS, "PLUS", False),
+        "blank": (State.BLANK, "MINUS", False),
+        "other": (State.ERROR, "MINUS", False),
+        "=": (State.ASSIGN, "MINUS", False),
+        "+": (State.PLUS, "MINUS", False),
         "-": (State.MINUS, "MINUS", False),
-        "numeric": (State.NUM, "NUM", False),
+        "numeric": (State.NUM, "MINUS", False),
+        "": (State.EOF, "MINUS", False),
     },
-    State.ERROR: {},  # no transitions here
-    State.EOF: {}  # no transitions here also
+    State.ERROR: {  # ERROR and EOF only have debug transitions
+        "blank": ("KEY 'blank' WENT TO ERROR", "NO EMIT", None),
+        "other": ("KEY 'other' WENT TO ERROR", "NO EMIT", None),
+        "=": ("KEY '=' WENT TO ERROR", "NO EMIT", None),
+        "+": ("KEY '+' WENT TO ERROR", "NO EMIT", None),
+        "-": ("KEY '-' WENT TO ERROR", "NO EMIT", None),
+        "numeric": ("KEY 'numeric' WENT TO ERROR", "NO EMIT", None),
+        "": ("KEY '' WENT TO ERROR", "NO EMIT", None),
+    },
+    State.EOF: {
+        "blank": ("KEY 'blank' WENT TO EOF", "NO EMIT", None),
+        "other": ("KEY 'other' WENT TO EOF", "NO EMIT", None),
+        "=": ("KEY '=' WENT TO EOF", "NO EMIT", None),
+        "+": ("KEY '+' WENT TO EOF", "NO EMIT", None),
+        "-": ("KEY '-' WENT TO EOF", "NO EMIT", None),
+        "numeric": ("KEY 'numeric' WENT TO EOF", "NO EMIT", None),
+        "": ("KEY '' WENT TO EOF", "NO EMIT", None),
+    }
 }
 
+"""
+1. open IO streams
+2. instantiate 1st character states
+3. enter main driver loop
 
-current_state = State.BLANK
-current_buffer = ""
-current_character = ""
+remember that we want to to flush the buffer not the
+character into output_file
 
+writing to output_file cases:
+(i) any state (that's not NUM and ASSIGN) transition
+(ii) for same state transitions only flush '+' and '-'
+(iii) for '=', we only flush that in twos, else, error
+(iv) NUM only gets flushed when we exit out of the NUM state
+
+_buffer format
+f'{_emit}   {_buffer}'
+"""
+
+# TODO: program execution
 input_file = io.open(sys.argv[1], "r")
-# output_file = io.open("output.txt", "w")
+output_file = io.open(sys.argv[2], "w")
+
+_char = input_file.read(1)  # get first char
+
+_buffer = ""  # instantiate char stream
+_buffer += _char  # flush the buffer not the char into output_file
+
+_state, _emit, _pushback = t_table[State.BLANK][gk(_char)]  # enter first state
 
 
-# TODO: finish driver loop
-while current_state not in (State.ERROR, State.EOF):
-    # read a character, assign current_character
-    current_character = input_file.read(1)
-    if current_character == "":
-        current_state = State.EOF
-        break
-    # do state transitions by invoking transition table
-    _state, _token, _pushback = t_table[current_state][group_char(
-        current_character)]
-    current_state = _state  # transition to next state
-    print(f"STATE: {current_state} CHAR: {current_character}")
-
+# while current_state != State.EOF:
+#     # read a character, assign current_character
+#     current_character = input_file.read(1)
+#     # print(f"{current_character} | | {current_state}")
+#
+#     # transition state via t_table, this also handles EOF
+#     _state, _token, _pushback = t_table[current_state][gk(current_character)]
+#     current_state = _state
+#
+#     # only the ff: go forward
+#     # State.BLANK, State.NUM, State.PLUS, State.MINUS and State.EOF
+#     output_file.write(f"{_token} | {current_character}\n")
 
 # program egress
-if current_state == State.ERROR:
-    print(f"Lexical Error reading character \"{current_character}\"")
+if _state == State.ERROR:
+    print(f"Lexical Error reading character \"{_char}\"")
 
 # close io streams since we're good software engineers
-# input_file.close()
-# output_file.close()
+input_file.close()
+output_file.close()
 
 sys.exit(0)
